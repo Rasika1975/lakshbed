e2c63a9c453e3f2bbba44968bf898e3346537ef2
@Rasika1975 <rasikagudulkar1008@gmail.com>
2026-06-18 11:40:01 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
          
           <li className="hover:text-blue-600 cursor-point
             
-
+\\
         </ul>
         {/* Logo */}
         <h1 className="text-2xl font-bold text-blue-600 cursor-pointer">
