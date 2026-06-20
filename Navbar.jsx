44b34fdf13fc8247367ee46e581a026f5c8b1515
@Rasika1975 <rasikagudulkar1008@gmail.com>
2026-06-20 11:10:53 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
           
 
 
-        {/* Button */}
+      
         <div className="hidden md:block">
           <button className="bg-blue-600 text-white px-5 py-2 rounded-xl hover:bg-blue-700 transition duration-300">
             Get Started
